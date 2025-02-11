45c6f143f7b19ddb33d7c6595bd4bdf4292aa21a
Piskis <piskis586@gmail.com>
2025-02-11 21:14:02 +0000

    Update Compiler.sh

--- Compiler.sh
+++ Compiler.sh
@@ -2,7 +2,7 @@
 
 files=$(ls *.asm 2>/dev/null)
 
-selected_file=$(dialog --title "File to compile" --menu "Files in current directory" 15 50 10 $(for f in $files; do echo "$f $f"; done) 2>&1 >/dev/tty) 
+selected_file=$(dialog --title "File for Compilation" --menu "Files in this directory" 15 50 10 $(for f in $files; do echo "$f $f"; done) 2>&1 >/dev/tty) 
 
 if [ $? -eq 0 ]; then 
   clear
@@ -11,16 +11,17 @@ if [ $? -eq 0 ]; then
   nasm -f elf64 "$selected_file" -o "$output_file.o" 
   ld "$output_file.o" -o "$output_file"
   clear 
-  Good_ending=$(dialog --title "Compiled as $output_file" --menu "What program need to do now?" 10 40 2 \
+  Good_ending=$(dialog --title "Compiled as $output_file" --menu "What to do next?" 11 65 4 \
     1 "Exit" \
-    2 "run $output_file" \
-    3 "Open in Vim" 2>&1 >/dev/tty) 
+    2 "Run ./$output_file" \
+    3 "Ater run program delete it and file .o" \
+    4 "Open in Vim" 2>&1 >/dev/tty) 
   case $Good_ending in
-    1) clear && echo "Exit";;
+    1) clear && echo "Exit without actions";;
     2) clear && ./"$output_file" ;;
-    3) clear && vim "$selected_file";;
+    3) clear && ./"$output_file" && rm "$output_file.o" "$output_file" ;;
+    4) clear && vim "$selected_file";;
   esac 
 else 
   clear
 fi
-
